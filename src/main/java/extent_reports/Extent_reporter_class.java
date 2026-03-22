d8241d2f512251191651acf49fb90c56c7719b83
Rohith Kumar Bathini <rohithbathini0@gmail.com>
2026-03-22 20:45:17 +0530

    excluded extent reports

--- src/main/java/extent_reports/Extent_reporter_class.java
+++ src/main/java/extent_reports/Extent_reporter_class.java
@@ -1,4 +1,4 @@
-package extent_reports;
+/*package extent_reports;
 
 import org.testng.ITestContext;
 import org.testng.ITestListener;
@@ -14,29 +14,29 @@ public class Extent_reporter_class implements ITestListener {
 	public ExtentReporter reporter;
 	public ExtentTest  test;
 
-	default void onTestStart(ITestResult result) {
+/*	default void onTestStart(ITestResult result) {
 		// not implemented
 		
 	}
 
-	/**
+	*//**
 	 * Invoked each time a test succeeds.
 	 *
 	 * @param result <code>ITestResult</code> containing information about the run
 	 *               test
 	 * @see ITestResult#SUCCESS
-	 */
+	 *//*
 	default void onTestSuccess(ITestResult result) {
 		// not implemented
 	}
 
-	/**
+	*//**
 	 * Invoked each time a test fails.
 	 *
 	 * @param result <code>ITestResult</code> containing information about the run
 	 *               test
 	 * @see ITestResult#FAILURE
-	 */
+	 *//*
 	default void onTestFailure(ITestResult result) {
 		// not implemented
 	}
@@ -53,15 +53,16 @@ public class Extent_reporter_class implements ITestListener {
 		
 	}
 
-	/**
+	*//**
 	 * Invoked after all the test methods belonging to the classes inside the
 	 * &lt;test&gt; tag have run and all their Configuration methods have been
 	 * called.
 	 *
 	 * @param context The test context
-	 */
+	 *//*
 	default void onFinish(ITestContext context) {
 		// not implemented
 	}
 
 }
+*/
